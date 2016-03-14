e3efd9484a7bcfb067418ace478825e2193711a6
John Orford <John.Orford@gmail.com>
2016-03-14 17:31:21 +0100

    login

---
+++ web/elm/LocationLinks.elm
@@ -0,0 +1,50 @@
+module LocationLinks where
+
+import Html exposing (Html, a, text, div)
+import Html.Events exposing (targetValue, onClick)
+import Html.Attributes exposing (href)
+
+import Signal exposing (Address)
+
+-- MODEL
+type alias Model = String
+
+init : String -> Model
+init location = location
+
+-- UPDATE
+type Action = Request | Login | Register | Logout
+
+update : Action -> Model
+update action =
+  case action of
+    Request ->
+      "request"
+    --once logged in go directly to request
+    Login ->
+      "login"
+    Register ->
+      "register"
+    Logout ->
+      "logout"
+
+-- VIEW
+view : Signal.Address Action -> Model -> Html
+view address model =
+  --if in ...
+  case model of
+    --reg location, show login link
+    "register" ->
+        a [ href "#", onClick address Login ] [ text "Login" ]
+    --login location, show reg link
+    "login" ->
+        a [ href "#", onClick address Register ] [ text "Register" ]
+    --request location show logout link
+    "request" ->
+        a [ href "#", onClick address Logout ] [ text "Logout" ]
+    --logout/default location show both register and login
+    _ ->
+      div [][
+          a [ href "#", onClick address Register ] [ text "Register" ]
+        , a [ href "#", onClick address Login ] [ text "Login" ]
+        ]
---
+++ web/elm/Router.elm
@@ -0,0 +1,170 @@
+module Router where
+
+import LoginForm exposing (init, update, view, loginRequestMailBox, Action)
+import RegisterForm exposing (init, update, view, registerRequestMailBox, Action)
+import RequestForm exposing (init, update, view, testMailBox, Action)
+
+import LocationLinks exposing (init, update, view, Action)
+
+import Html exposing (a, text, Html, div, button, span)
+import Html.Attributes exposing (href)
+import Html.Events exposing (targetChecked, on, onClick)
+
+import Http exposing (get, url)
+
+import Task exposing (toMaybe, andThen)
+
+import Effects exposing (Effects)
+
+import Signal exposing (Address)
+
+import List
+
+
+
+--********************************************************************************
+--********************************************************************************
+-- MODEL
+type alias Model =
+    {
+      data : RequestForm.Model
+    , userLogin : LoginForm.Model
+    , userRegister : RegisterForm.Model
+    , location: LocationLinks.Model
+    }
+
+init : (Model, Effects Action)
+init =
+  let
+    (request, requestFx) = RequestForm.init "Yahoo" "INDEX_VIX" False
+    (login, loginFx) = LoginForm.init "" ""
+    (register, registerFx) = RegisterForm.init "" "" ""
+    locationLinks = LocationLinks.init ""
+  in
+    ( Model request login register locationLinks
+    , Effects.batch
+        [ Effects.map Login loginFx
+        , Effects.map Request requestFx
+        ]
+    )
+
+
+--********************************************************************************
+--********************************************************************************
+-- UPDATE
+--act like a router, sending to different forms based on actions
+--
+type Action
+    = Login LoginForm.Action
+    | Request RequestForm.Action
+    | Register RegisterForm.Action
+    | ChangeLocation LocationLinks.Action
+
+update : Action -> Model -> (Model, Effects Action)
+update action model =
+  case action of
+    Request input ->
+      let
+        (newData, fx) = RequestForm.update input model.data
+      in
+        ( { model | data = newData }
+        , Effects.map Request fx
+        )
+    --action to capture response from outside world
+    --take action update state
+    --then send on another action to update whichever form...
+    Register input ->
+      let
+        (newUser, fx) = RegisterForm.update input model.userRegister
+      in
+        ( { model |
+              userRegister = newUser
+            , location =
+              --forward page?
+              case newUser.response of
+                "OK" ->
+                  LocationLinks.update LocationLinks.Request
+                _ ->
+                  model.location
+          }
+        , Effects.map Register fx
+        )
+    Login input ->
+      let
+        (newUser, fx) = LoginForm.update input model.userLogin
+      in
+        ( { model |
+            userLogin = newUser
+          --forward page?
+          , location = forwardOnLogin newUser.response model.location
+          }
+        , Effects.map Login fx
+        )
+    --reset to original state
+    ChangeLocation input ->
+      let
+        newLocation = LocationLinks.update input
+      in
+        --forward?
+        case newLocation of
+          "logout" ->
+            init
+          _ ->
+            ( { model |
+              location = newLocation
+              }
+            , Effects.none
+            )
+
+--forwardOnLogin : String -> String
+
+forwardOnLogin : String -> String -> LocationLinks.Model
+forwardOnLogin response currentLocation =
+  case response of
+    "OK" ->
+      LocationLinks.update LocationLinks.Request
+    _ ->
+      currentLocation
+
+
+
+--********************************************************************************
+--********************************************************************************
+-- VIEW
+view : Signal.Address Action -> Model -> Html
+view address model =
+  --keep track of location in model
+  --if not logged in, token is blank
+  --  then goto login page
+  --if register link is clicked, go to register form
+  --if login link is click, go to login form
+  --if logout link is clicked, go to login page
+  --default is login
+  case model.location of
+    --if page is...
+    "request" ->
+      div [][
+          LocationLinks.view (Signal.forwardTo address ChangeLocation) model.location
+        , RequestForm.view (Signal.forwardTo address Request) model.data
+        ]
+    "register" ->
+      div [][
+          LocationLinks.view (Signal.forwardTo address ChangeLocation) model.location
+        , RegisterForm.view (Signal.forwardTo address Register) model.userRegister
+        ]
+    "login" ->
+      div [][
+          LocationLinks.view (Signal.forwardTo address ChangeLocation) model.location
+        , LoginForm.view (Signal.forwardTo address Login) model.userLogin
+        ]
+    _ ->
+      div [][
+          LocationLinks.view (Signal.forwardTo address ChangeLocation) model.location
+        ]
+
+
+
+
+--********************************************************************************
+--********************************************************************************
+-- EFFECTS
